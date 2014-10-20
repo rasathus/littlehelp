641935f74e4ab8376dc17952d6a05e2fb4554282
Chris Fane <chris@Chris-MacBook-Pro.local>
2014-10-20 23:09:43 +0100

    First working draft of littlehelp

---
+++ .littlehelp/littlehelp.py
@@ -0,0 +1,77 @@
+import logging
+import os
+import signal
+import sys
+import subprocess
+
+from time import sleep
+
+from pync import Notifier
+
+from fsevents import Observer, Stream
+
+import daemon
+
+project_path, junk = os.path.split(os.path.dirname(os.path.realpath(__file__)))
+littlehelp_script = os.path.join(project_path, 'littlehelp')
+del junk
+
+logging.basicConfig(filename=os.path.join(project_path, '.littlehelp/littlehelp.log'),level=logging.INFO)
+
+ignored_suffix = ['.DS_Store','.swp',"littlehelp.log"]
+
+def file_event_callback(event):
+    """This is the function being called when an event on a file is detected."""
+    # just worry about the modification events.
+    if event.mask == 2:
+        for suffix in ignored_suffix:
+            if event.name.endswith(suffix):
+                print "ignoring file: {0}".format(event.name)
+                break
+        else:
+            file_path, file_name = os.path.split(event.name)
+            logging.info("Mask: %s, Cookie: %s, Name: %s" % (event.mask, event.cookie, event.name))
+            try:
+                subprocess.call('{0} action {1} {2}'.format(littlehelp_script, file_path, file_name), shell=True)
+            except:
+                logging.exception("Exceptiong whilst running action '{0} action {1} {2}'".format(littlehelp_script, file_path, file_name))
+            Notifier.notify('change to file: {0}, triggered action'.format(file_name), title='LittleHelp')
+
+def main():
+
+    def sigterm_handler(_signo, _stack_frame):
+        try:
+            Notifier.notify('Unregistering fs watch', title='LittleHelp')
+        except:
+            pass
+        logging.info("Sigterm handler called")
+        observer.unschedule(stream)
+        observer.stop()
+        observer.join()
+        try:
+            Notifier.notify('Unregistered fs watch', title='LittleHelp')
+        except:
+            pass
+        sys.exit(0)
+
+    try:
+        observer = Observer()
+        stream = Stream(file_event_callback, project_path, file_events=True)
+        observer.schedule(stream)
+        observer.start()
+
+        signal.signal(signal.SIGTERM, sigterm_handler)
+
+        while True:
+            sleep(0.1)
+#        observer.join() # This will block while watching.
+    except:
+        logging.exception("Unhandled exception")
+
+if __name__ == "__main__":
+    logging.debug("Project Path: {0}".format(project_path))
+    logging.debug("Littlehelp Script: {0}".format(littlehelp_script))
+
+    with daemon.DaemonContext():
+        main()
+    #main()
